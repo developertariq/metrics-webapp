a5891a968c59aa8f677fd9a1b44b9c6c0394a1e6
developertariq <dev.tariqul.islam@gmail.com>
2023-02-08 11:56:39 +0600

    Update app.js file for route categories

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Continents from './pages/Continents';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import store from './Redux/configureStore';
 import './App.css';
+import Countries from './pages/Countries';
 
 function App() {
   return (
-    <div className="App" />
+    <Provider store={store}>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Continents />} />
+            <Route index path="/countries" element={<Countries />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
