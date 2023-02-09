f5af8255e794a3c2756424148c113ccbf804c66f
developertariq <dev.tariqul.islam@gmail.com>
2023-02-10 05:33:02 +0600

    Create and implement action to change title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,52 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog } from '@fortawesome/free-solid-svg-icons';
-import Button from 'react-bootstrap/Button';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
+import './Header.css';
+import { FaChevronLeft } from 'react-icons/fa';
+import { IoSettingsSharp } from 'react-icons/io5';
+import { BsMicFill } from 'react-icons/bs';
+import { updateTitle } from '../Redux/Title/titleReducer';
 
-const Header = () => (
-  <header className="header" data-testid="header">
-    <Button>&lt;</Button>
-    <h1 className="title">
-      Weather App
-    </h1>
-    <FontAwesomeIcon icon={faCog} size="lg" />
-  </header>
-);
+const Header = () => {
+  const navigate = useNavigate();
+  const title = useSelector((state) => state.title);
+  const dispatch = useDispatch();
+  const location = useLocation();
+
+  useEffect(() => {
+    let newTitle = '';
+
+    switch (location.pathname) {
+      case '/':
+        newTitle = 'World Population';
+        break;
+      case '/continent':
+        newTitle = 'Continental View';
+        break;
+      case '/country':
+        newTitle = 'Country/State  Information';
+        break;
+      default:
+        newTitle = 'Page Not Found';
+        break;
+    }
+
+    dispatch(updateTitle(newTitle));
+  }, [location, dispatch]);
+
+  return (
+    <header className="header" data-testid="header">
+      <div className="header-left">
+        <FaChevronLeft className="back-button" onClick={() => navigate(-1)} />
+        <h2>2023</h2>
+      </div>
+      {title.title}
+      <div className="header-right">
+        <BsMicFill style={{ fontSize: '20px' }} />
+        <IoSettingsSharp style={{ fontSize: '20px' }} />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
