c34af7c49750586f2c68cb1f64bcf9047df54056
developertariq <dev.tariqul.islam@gmail.com>
2023-02-10 05:35:10 +0600

    Update configureStore.js

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import continentsSlice from './Continent/continentsSlice';
 import countriesSlice from './Country/countriesSlice';
+import titleReducer from './Title/titleReducer';
 
 const reducer = combineReducers({
   continents: continentsSlice,
   countries: countriesSlice,
+  title: titleReducer,
 });
 
 const store = configureStore({
