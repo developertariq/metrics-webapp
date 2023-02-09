ea805ce04a0175fd0f2fc9c8022338a1debb395c
developertariq <dev.tariqul.islam@gmail.com>
2023-02-10 05:34:33 +0600

    Load and display countries from api

--- src/Redux/Country/countriesSlice.js
+++ src/Redux/Country/countriesSlice.js
@@ -1,35 +1,43 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const COUNTRY_URL = 'https://restcountries.com/v3.1/region/';
+// const MAP_URL = 'https://raw.githubusercontent.com/djaiss/mapsicon/33ba28808f8d32b5bae0ffada9cadd07073852e1/all/';
+const COUNTRY_MAP_URL = 'https://raw.githubusercontent.com/djaiss/mapsicon/master/all/';
 const initialState = {
   countries: [],
   status: 'idle',
   error: null,
 };
-const FETCH_URL = 'https://restcountries.com/v3.1/';
-let filterContinent;
-export const getCountries = createAsyncThunk('weather/country/FETCH_DATA', async (region, continent) => {
-  const response = await axios.get(`${FETCH_URL}region/${region}`);
-  filterContinent = continent;
-  console.log(`${FETCH_URL}region/${region}`);
-  return response.data;
+
+export const getCountries = createAsyncThunk('weather/country/FETCH_DATA', async (obj) => {
+  const { region, name } = obj;
+  const response = await axios.get(`${COUNTRY_URL}${region}`);
+
+  return response.data.filter((country) => country.continents[0] === name);
 });
 
-const matchCountries = (data) => {
-  const newContries = data.map((country) => ({
-    id: country.fifa,
-    path: `/${country.name.common}`,
-    text: country.name.common,
-    area: country.area,
-    capital: country.capital,
-    population: country.population,
-    flag: country.flags,
-    map: country.maps,
-    region: country.region,
-    continent: country.continents[0],
-  }));
+const countryInformation = (data) => {
+  const newContries = data.filter((country) => country.independent)
+    .map((country) => ({
+      id: country.cca3,
+      path: `/${country.name.common}`,
+      name: country.name.common,
+      official: country.name.official,
+      area: country.area,
+      capital: country.capital,
+      population: country.population,
+      flag: country.flags.png,
+      map: `${COUNTRY_MAP_URL}${country.cca2.toLowerCase()}/vector.svg`,
+      region: country.region,
+      subregion: country.subregion,
+      continent: country.continents[0],
+      currencies: country.currencies,
+      latlng: country.latlng,
+      languages: country.languages,
+    }));
 
-  return newContries.filter((country) => country.continent === filterContinent);
+  return newContries.sort((a, b) => a.name.localeCompare(b.name));
 };
 
 const countriesSlice = createSlice({
@@ -43,7 +51,7 @@ const countriesSlice = createSlice({
       })
       .addCase(getCountries.fulfilled, (state, action) => {
         state.status = 'succeeded';
-        state.countries = matchCountries(action.payload);
+        state.countries = countryInformation(action.payload);
       })
       .addCase(getCountries.rejected, (state, action) => {
         state.status = 'failed';
--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -1,23 +1,36 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import { getCountries } from '../Redux/Country/countriesSlice';
+import Country from '../components/Country';
+import PageHeader from '../components/PageHeader';
 
 const Countries = () => {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const location = useLocation();
-  const { search } = location;
-  const params = new URLSearchParams(search);
-  const continent = params.get('continent');
-  const region = params.get('region');
+  const searchParams = new URLSearchParams(location.search);
+  const continent = JSON.parse(searchParams.get('continent') || '{}');
+
+  const {
+    region, map, population, name, noOfCountries,
+  } = continent;
   const { countries, status, error } = useSelector((state) => state.countries);
-  console.log(continent, region, countries);
+
   useEffect(() => {
-    if (countries.length === 0 || region !== countries[0].region) {
-      dispatch(getCountries(region, continent));
+    if (
+      countries.length === 0
+      || region !== countries[0].region
+      || name !== countries[0].continent
+    ) {
+      dispatch(getCountries({ region, name }));
     }
   }, [countries, dispatch]);
+
+  const handleCountryClick = (country) => {
+    navigate(`/country?country=${JSON.stringify(country)}`);
+  };
 
   if (status === 'loading') {
     return <div>Loading...</div>;
@@ -33,11 +46,32 @@ const Countries = () => {
   }
 
   return (
-    <div>
+    <div className="main-container">
+      <PageHeader
+        map={map}
+        name={name}
+        population={population}
+      />
+      <div className="section-title">
+        <h4>
+          { noOfCountries }
+          {' '}
+          Countries in
+          {' '}
+          { name }
+        </h4>
+      </div>
       <Navbar>
         <Nav className="navbar">
-          {countries.map((link) => (
-            <Nav.Link className="nav-item" key={link.id} to={link.path}>{link.text}</Nav.Link>
+          {countries.map((country) => (
+            <Nav.Link
+              className="nav-item"
+              key={country.id}
+              to={country.path}
+              onClick={() => { handleCountryClick(country); }}
+            >
+              <Country country={country} />
+            </Nav.Link>
           ))}
         </Nav>
       </Navbar>
